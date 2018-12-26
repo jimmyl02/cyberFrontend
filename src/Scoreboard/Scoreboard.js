45250ac28bb46db1d2dd5050f08a09614cd87110
Jimmy Li <jimmyli2002@gmail.com>
2018-12-25 23:29:08 -0800

    Initial commit

---
+++ src/Scoreboard/Scoreboard.js
@@ -0,0 +1,133 @@
+import React, { Component } from "react";
+import injectSheet from "react-jss";
+import moment from "moment";
+import { styles } from "./styles";
+
+import { Input, Button, Table } from "antd";
+
+class unstyledScoreboard extends Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      data: [],
+      name: "",
+      imageName: ""
+    };
+
+  }
+
+  async componentDidMount() {
+
+    fetch("http://107.170.200.206/api/user/getScores")
+      .then((results) => {
+        return results.json();
+      })
+      .then((jsonResults) => {
+        let newData = [];
+
+        for(let entry in jsonResults){
+          let data = jsonResults[entry];
+          data["key"] = entry;
+          const dateString = new Date(data["startTime"]);
+          data["startTime"] = moment(dateString).format('MMMM Do YYYY, h:mm:ss a');
+          newData.push(data);
+        }
+
+        //console.log(newData);
+
+        this.setState({data: jsonResults});
+      });
+
+  }
+
+  onChangeName = (e) => {
+    this.setState({ name: e.target.value });
+  }
+
+  onChangeImageName = (e) => {
+    this.setState({ imageName: e.target.value });
+  }
+
+  onSubmit = (e) => {
+    let addedQueryString = "?";
+    if(this.state.name){
+      addedQueryString += "name=" + this.state.name + "&";
+    }
+    if(this.state.imageName){
+      addedQueryString += "imageName=" + this.state.imageName;
+    }
+    fetch("http://107.170.200.206/api/user/getScores" + addedQueryString)
+      .then((results) => {
+        return results.json();
+      })
+      .then((jsonResults) => {
+        let newData = [];
+
+        for(let entry in jsonResults){
+          let data = jsonResults[entry];
+          data["key"] = entry;
+          const dateString = new Date(data["startTime"]);
+          data["startTime"] = moment(dateString).format('MMMM Do YYYY, h:mm:ss a');
+          newData.push(data);
+        }
+
+        //console.log(newData);
+
+        this.setState({data: jsonResults});
+      });
+  }
+
+  render() {
+
+    const { classes } = this.props;
+
+    const columns = [{
+      title: "Name",
+      dataIndex: "name",
+      key: "name"
+    }, {
+      title: "Image Name",
+      dataIndex: "imageName",
+      key: "imageName"
+    }, {
+      title: "Score",
+      dataIndex: "score",
+      key: "score"
+    }, {
+      title: "Start Time",
+      dataIndex: "startTime",
+      key: "startTime"
+    }];
+
+    return (
+      <div className={classes.scoreboard}>
+        <header className="scoreboardHeader">
+          <div className={classes.header}>
+            <h1>Troy High School Cyber Scoreboard</h1>
+          </div>
+        </header>
+        <div className={classes.parameters}>
+          <Input
+            placeholder="Enter your name"
+            value={this.state.name}
+            onChange={this.onChangeName}
+            className={classes.parameterItem}
+          />
+          <Input
+            placeholder="Enter the image name"
+            value={this.state.imageName}
+            onChange={this.onChangeImageName}
+            className={classes.parameterItem}
+          />
+          <Button type="primary" onClick={this.onSubmit} className={classes.parameterItem}>Submit</Button>
+        </div>
+        <Table dataSource={this.state.data} columns={columns}/>
+      </div>
+    );
+  }
+}
+
+export const Scoreboard = injectSheet(styles)(unstyledScoreboard);
---
+++ src/Scoreboard/styles.js
@@ -0,0 +1,22 @@
+export const styles = {
+    scoreboard: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        padding: "1em"
+    },
+    header: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center"
+    },
+    parameters: {
+        display: "flex",
+        flexDirection: "row",
+        marginBottom: "1em"
+    },
+    parameterItem: {
+        marginLeft: "1em",
+        marginRight: "1em"
+    }
+}
