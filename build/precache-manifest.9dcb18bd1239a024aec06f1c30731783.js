283d70654d1f72470b6608a553909572bc29fb5c
Jimmy Li <jimmyli2002@gmail.com>
2018-12-25 23:57:17 -0800

    Added build folder

---
+++ build/precache-manifest.9dcb18bd1239a024aec06f1c30731783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fe502edad0e71a9b8e89",
+    "url": "/static/js/main.fe502eda.chunk.js"
+  },
+  {
+    "revision": "6b44bd9f0487198f0f5c",
+    "url": "/static/js/1.6b44bd9f.chunk.js"
+  },
+  {
+    "revision": "fe502edad0e71a9b8e89",
+    "url": "/static/css/main.ad547410.chunk.css"
+  },
+  {
+    "revision": "6b44bd9f0487198f0f5c",
+    "url": "/static/css/1.f379013b.chunk.css"
+  },
+  {
+    "revision": "4e34977dc1f2fa6d99affd9770d62bdb",
+    "url": "/index.html"
+  }
+];
